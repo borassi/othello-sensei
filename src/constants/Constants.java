22ca87bf4a12f4519aef5143fe4d2d29dbe7e82d
michele <michele.borassi@gmail.com>
2020-10-11 17:26:03 +0200

    Small changes to output.

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -36,6 +36,6 @@ public class Constants {
   public final static int TRY_SC_LAST_3_NO_CORNER = 600;
   public final static int TRY_SC_LAST_3_CORNER = 1600;
   
-  public final static double MIN_COST_LEAF = 1E-4;
-  public final static double LAMBDA = -10;
+  public final static double MIN_COST_LEAF = 1E-5;
+  public final static double LAMBDA = -6;
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -370,7 +370,7 @@ public class Main implements Runnable {
 //      annotations.disproofNumberNextEval = child.getProofNumberNextEval();
       annotations.otherAnnotations =
         Utils.prettyPrintDouble(child.getEvalGoal() / 100) + "\n"
-        + Utils.prettyPrintDouble(1 - child.probGreaterEqualEvalGoal) + " " + Utils.prettyPrintDouble(1 - child.probStrictlyGreaterEvalGoal) + "\n"
+        + Utils.prettyPrintDouble(1 - child.probGreaterEqualEvalGoal) + " " + Utils.prettyPrintDouble(child.probStrictlyGreaterEvalGoal) + "\n"
         + Utils.prettyPrintDouble(current.logDerivativePlayerVariates(child) + child.minLogDerivativeOpponentVariates) + " "
         + Utils.prettyPrintDouble(current.logDerivativeOpponentVariates(child) + child.minLogDerivativePlayerVariates) + "\n"
         + Utils.prettyPrintDouble(Math.exp(current.logDerivativePlayerVariates(child))) + " "
