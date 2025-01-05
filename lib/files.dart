a377965c53be7b69f5fa8e4db66c2324c74e20ba
Michele Borassi <michele.borassi@gmail.com>
2025-01-05 16:28:36 +0100

    Change files version to make sure we store the new Thor archive.

--- lib/files.dart
+++ lib/files.dart
@@ -21,7 +21,7 @@ import 'package:flutter/services.dart';
 import 'package:othello_sensei/state.dart';
 import 'package:path/path.dart';
 
-const String assetVersion = "8";
+const String assetVersion = "9";
 
 String localAssetPath() {
   return join(GlobalState.localPath, "assets");
