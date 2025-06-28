ea2383f5ab2d0107369c0d57f5839a8cff831d45
Michele Borassi <michele.borassi@gmail.com>
2025-06-28 14:17:13 +0200

    Update Flutter version and change Android to Kotlin.

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,64 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystorePropertiesFile = rootProject.file("key.properties")
+val keystoreProperties = Properties().apply {
+    load(FileInputStream(keystorePropertiesFile))
+}
+
+android {
+    namespace = "com.othellosensei.app"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.othellosensei.app.dev"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties ["keyAlias"] as String
+            keyPassword = keystoreProperties ["keyPassword"] as String
+            storeFile = file(keystoreProperties ["storeFile"])
+            storePassword = keystoreProperties ["storePassword"] as String
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
+    externalNativeBuild {
+        cmake {
+            path = file("../../engine/CMakeLists.txt")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
