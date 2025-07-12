698952e1123c4ae074f968afdb0f659d608e7b04
Michele Borassi <michele.borassi@gmail.com>
2025-07-12 06:18:13 +0200

    Move GamesList from source.h to game.h.

--- engine/thor/game.cpp
+++ engine/thor/game.cpp
@@ -16,6 +16,38 @@
  
  #include "game.h"
 
+void SortAndCutExamples(int max_games, std::vector<Game>& examples) {
+  std::sort(examples.begin(), examples.end(), CompareGamesByHash);
+  if (examples.size() > max_games) {
+    examples.erase(examples.begin() + max_games, examples.end());
+  }
+}
+
+void GamesList::Merge(const GamesList& other) {
+  num_games += other.num_games;
+  examples.insert(examples.end(), other.examples.begin(), other.examples.end());
+  SortAndCutExamples(max_games, examples);
+  for (const auto& [key, value] : other.next_moves) {
+    next_moves[key] += value;
+  }
+}
+
+void GamesList::Rotate(const Sequence& target) {
+  if (num_games == 0) {
+    return;
+  }
+  assert(examples[0].Moves().Size() >= target.Size());
+  int transposition = target.GetTransposition(examples[0].Moves().Subsequence(target.Size()));
+  if (transposition == 0) {
+    return;
+  }
+  std::unordered_map<Square, int> old_next_moves(next_moves.begin(), next_moves.end());
+  next_moves.clear();
+  for (const auto& [move, games] : old_next_moves) {
+    next_moves[TransposeMove(move, transposition)] = games;
+  }
+}
+
 std::string Game::ToString() const {
   std::stringstream result;
   result << *black_ << " " << (int) score_ << " - " << (64-score_) << " " << *white_ << " (" << *tournament_ << " " << year_ << "): " << Moves() << " " << Priority() << "\n";
