5ea087c1f0ed26f9a27fecf1a1351c035f3a7725
Michele Borassi <michele.borassi@gmail.com>
2025-09-21 17:27:26 +0200

    Update the asset version.

--- lib/files.dart
+++ lib/files.dart
@@ -20,7 +20,7 @@ import 'package:flutter/services.dart';
 import 'package:othello_sensei/state.dart';
 import 'package:path/path.dart';
 
-const String assetVersion = "16";
+const String assetVersion = "17";
 
 String localAssetPath() {
   return join(GlobalState.localPath, "assets");
